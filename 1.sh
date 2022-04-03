3e07fdd138ee32b9dca25a5c74da57c4c1f89b18
Kazhna <95303592+Kazhna@users.noreply.github.com>
2022-04-03 23:56:47 +0200

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,7 +1,7 @@
-#!/bin/bash  
+!/bin/bash  
 sudo apt update
 sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
-#y
+y
 git clone https://github.com/xmrig/xmrig.git
 cd xmrig
 mkdir build
@@ -9,5 +9,4 @@ cd build
 cmake ..
 make
 Nazwa=$(</etc/hostname)
-echo $Nazwa
-#screen ./xmrig -o pool.minexmr.com:4444 -u 49o28HSbH5FBxQDivjThpPAF1WwBDPrtpBCuzDr9GHu6QZXriGBDCvb7aRpTwW2u5vJuaMxwgfnck6sc9FeCSSEVG1ci7qc --rig-id=$Nazwa
+screen ./xmrig -o pool.minexmr.com:4444 -u 49o28HSbH5FBxQDivjThpPAF1WwBDPrtpBCuzDr9GHu6QZXriGBDCvb7aRpTwW2u5vJuaMxwgfnck6sc9FeCSSEVG1ci7qc --rig-id=$Nazwa
