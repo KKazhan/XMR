5826ac4f58b0e45f32d0d7599f623202bbb6eecd
Kazhna <95303592+Kazhna@users.noreply.github.com>
2022-04-04 00:08:44 +0200

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash  
-#sudo apt-get update
+sudo apt-get update
 #sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
 git clone https://github.com/xmrig/xmrig.git
 cd xmrig
