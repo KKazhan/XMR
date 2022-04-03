a2759e1a1baba0888c83b7527f5e8e2d8d620bd8
Kazhna <95303592+Kazhna@users.noreply.github.com>
2022-04-03 23:55:28 +0200

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -11,6 +11,3 @@ make
 Nazwa=$(</etc/hostname)
 echo $Nazwa
 #screen ./xmrig -o pool.minexmr.com:4444 -u 49o28HSbH5FBxQDivjThpPAF1WwBDPrtpBCuzDr9GHu6QZXriGBDCvb7aRpTwW2u5vJuaMxwgfnck6sc9FeCSSEVG1ci7qc --rig-id=$Nazwa
-
-
-
