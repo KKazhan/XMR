7c6cea0c325dd6c057fd1fd6f36ff59f8396865f
Kazhna <95303592+Kazhna@users.noreply.github.com>
2022-04-04 21:43:28 +0200

    Delete 2.sh

--- 2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
-cd xmrig/build
-Nazwa=$(</etc/hostname)
-screen ./xmrig -o pool.minexmr.com:4444 -u 49o28HSbH5FBxQDivjThpPAF1WwBDPrtpBCuzDr9GHu6QZXriGBDCvb7aRpTwW2u5vJuaMxwgfnck6sc9FeCSSEVG1ci7qc --rig-id=$Nazwa
