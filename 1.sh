362456e734c3dba7d41b123ccc6cf8b14e0f68f0
Kazhna <95303592+Kazhna@users.noreply.github.com>
2022-04-04 20:52:42 +0200

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,12 +1,11 @@
 #!/bin/bash  
 sudo apt update
 sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
-sleep 20
-echo "y"
 git clone https://github.com/xmrig/xmrig.git
 cd xmrig
 mkdir build
 cd build
+screen
 cmake ..
 make
 Nazwa=$(</etc/hostname)
