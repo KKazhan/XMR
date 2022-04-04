84ee86114421ef161425960f68265565ab9f6a7e
Kazhna <95303592+Kazhna@users.noreply.github.com>
2022-04-04 22:32:18 +0200

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,10 +1,3 @@
 #!/bin/bash  
-git clone https://github.com/xmrig/xmrig.git
-cd xmrig
-mkdir build
-cd build
-cmake ..
-make
 cd /home/ubuntu/xmrig/build/
-Nazwa=$(</etc/hostname)
 screen sudo ./xmrig -o pool.minexmr.com:4444 -u 49o28HSbH5FBxQDivjThpPAF1WwBDPrtpBCuzDr9GHu6QZXriGBDCvb7aRpTwW2u5vJuaMxwgfnck6sc9FeCSSEVG1ci7qc --rig-id=$Nazwa
