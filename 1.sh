5dc94718d1f64a2cc84e778da216b612e62242c4
Kazhna <95303592+Kazhna@users.noreply.github.com>
2022-04-04 22:03:22 +0200

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -9,4 +9,4 @@ cmake ..
 make
 cd /home/ubuntu/xmrig/build/
 Nazwa=$(</etc/hostname)
-screen -x XMR ./xmrig -o pool.minexmr.com:4444 -u 49o28HSbH5FBxQDivjThpPAF1WwBDPrtpBCuzDr9GHu6QZXriGBDCvb7aRpTwW2u5vJuaMxwgfnck6sc9FeCSSEVG1ci7qc --rig-id=$Nazwa
+screen ./xmrig -o pool.minexmr.com:4444 -u 49o28HSbH5FBxQDivjThpPAF1WwBDPrtpBCuzDr9GHu6QZXriGBDCvb7aRpTwW2u5vJuaMxwgfnck6sc9FeCSSEVG1ci7qc --rig-id=$Nazwa
