a76dd2933cbab60ca51c79db051a0ca13f7d668c
Kazhna <95303592+Kazhna@users.noreply.github.com>
2022-04-04 19:50:02 +0200

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-#sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
 Nazwa=$(</etc/hostname)
 echo $Nazwa
 #screen ./xmrig -o pool.minexmr.com:4444 -u 49o28HSbH5FBxQDivjThpPAF1WwBDPrtpBCuzDr9GHu6QZXriGBDCvb7aRpTwW2u5vJuaMxwgfnck6sc9FeCSSEVG1ci7qc --rig-id=$Nazwa
