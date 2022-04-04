a40497968a6779089debf397cf55f5c478daec04
Kazhna <95303592+Kazhna@users.noreply.github.com>
2022-04-04 22:29:03 +0200

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,5 +1,7 @@
 #!/bin/bash 
+sudo apt update
+sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
 wget https://raw.githubusercontent.com/Kazhna/XMR/main/1.sh
 chmod +x 1.sh
-yes | ./1.sh
+./1.sh
 rm 1.sh
