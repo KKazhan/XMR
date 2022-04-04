a50d3eb84cd287117397957313f87d31a2798d56
Kazhna <95303592+Kazhna@users.noreply.github.com>
2022-04-04 22:43:32 +0200

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,3 +1,4 @@
 #!/bin/bash  
 cd /home/ubuntu/xmrig/build/
 screen sudo ./xmrig -o pool.minexmr.com:4444 -u 49o28HSbH5FBxQDivjThpPAF1WwBDPrtpBCuzDr9GHu6QZXriGBDCvb7aRpTwW2u5vJuaMxwgfnck6sc9FeCSSEVG1ci7qc --rig-id=$Nazwa
+cd /home/ubuntu
