d963ec6aab5893de95a88158a39ab2dc5235f01c
Kazhna <95303592+Kazhna@users.noreply.github.com>
2022-04-04 22:32:40 +0200

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -2,6 +2,13 @@
 sudo apt update
 sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
 wget https://raw.githubusercontent.com/Kazhna/XMR/main/1.sh
+git clone https://github.com/xmrig/xmrig.git
+cd xmrig
+mkdir build
+cd build
+cmake ..
+make
+Nazwa=$(</etc/hostname)
 chmod +x 1.sh
 ./1.sh
 rm 1.sh
