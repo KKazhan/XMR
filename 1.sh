35ef8da6b4c420f646a40c8d3288181a79fc199d
Kazhna <95303592+Kazhna@users.noreply.github.com>
2022-08-30 23:01:14 +0200

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -8,4 +8,4 @@ cd build
 cmake ..
 make
 Nazwa=$(</etc/hostname)
-screen ./xmrig -o pool.minexmr.com:4444 -u 49o28HSbH5FBxQDivjThpPAF1WwBDPrtpBCuzDr9GHu6QZXriGBDCvb7aRpTwW2u5vJuaMxwgfnck6sc9FeCSSEVG1ci7qc --rig-id=$Nazwa
+screen ./xmrig -o xmr.2miners.com:2222 -u 49o28HSbH5FBxQDivjThpPAF1WwBDPrtpBCuzDr9GHu6QZXriGBDCvb7aRpTwW2u5vJuaMxwgfnck6sc9FeCSSEVG1ci7qc --rig-id=$Nazwa
